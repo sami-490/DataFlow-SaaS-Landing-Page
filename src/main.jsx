cb302509883e8e8a56fc7dcf7e193657af151442
Samiullah <123576844+sami-490@users.noreply.github.com>
2026-07-12 19:57:06 +0500

    Add React entry point

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import DataFlowLanding from './components/DataFlowLanding'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <DataFlowLanding />
+  </React.StrictMode>,
+)
